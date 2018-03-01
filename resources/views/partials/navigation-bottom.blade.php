88ac9351c81838ceab48e9535779ef5ace1dd02d
idriesalbender <idries_albender@hotmail.com>
2018-03-01 10:57:04 +0100

    Center nav items

--- resources/views/partials/navigation-bottom.blade.php
+++ resources/views/partials/navigation-bottom.blade.php
@@ -2,7 +2,7 @@
     <div class="text-center">
         <nav class="nav-primary d-flex justify-content-center">
             @if (has_nav_menu('bottom_navigation'))
-                {!! wp_nav_menu(['theme_location' => 'bottom_navigation', 'menu_class' => 'nav']) !!}
+                {!! wp_nav_menu(['theme_location' => 'bottom_navigation', 'menu_class' => 'nav justify-content-center']) !!}
             @endif
         </nav>
     </div>
