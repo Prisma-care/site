d4fe83aa0bbf9ba2c0c891822dee39f068b52cf6
idriesalbender <idries_albender@hotmail.com>
2018-03-01 10:43:07 +0100

    Create bottom navigation

---
+++ resources/views/partials/navigation-bottom.blade.php
@@ -0,0 +1,9 @@
+<div class="banner">
+    <div class="text-center">
+        <nav class="nav-primary d-flex justify-content-center">
+            @if (has_nav_menu('bottom_navigation'))
+                {!! wp_nav_menu(['theme_location' => 'bottom_navigation', 'menu_class' => 'nav']) !!}
+            @endif
+        </nav>
+    </div>
+</div>
