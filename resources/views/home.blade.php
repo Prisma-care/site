3b56e83596e5902537e2cbc09dfff9f9fa68d462
idriesalbender <idries_albender@hotmail.com>
2018-03-15 17:59:51 +0100

    Rework home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,18 @@
 
       <div class="col-lg-10 col-xl-8 text-center">
 
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/Xka4MPtLjoU?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-        </div>  
+        <p class="lead">Prisma helpt mensen met dementie, hun geliefden en verzorgers om een goeie babbel te hebben.</p>
 
         <br>
-        <p class="lead">De gratis Prisma app helpt mensen met dementie, hun geliefden en verzorgers om een goeie babbel te hebben.</p>
 
-        <a href="https://play.google.com/store/apps/details?id=care.prisma.familyapp"><img src="@asset('images/cta-googleplay.png')" alt="Download de app op Google Play" class="playstore-link mt-4 mb-3"></a> 
-        <p class="small">De iPhone en iPad versie komt er aan</p>
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/Xka4MPtLjoU?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+        </div>
+
+        <br>
+
+        <a class="btn btn-primary btn-lg font-weight-bold mb-3" href="#" role="button">Aan de slag</a>
+        <p class="small"><a href="#">Prisma TV</a> is volledig gratis en beschikbaar voor alle toestellen.</p>
       </div>
 
     </div>
@@ -45,44 +48,34 @@
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-md-6 col-lg-7">
-        <img src="@asset('images/tour-hulahoop.jpg')" alt="Bewaar jullie herinneringen in een digitaal fotoalbum" class="img-fluid">
-      </div>
-      <div class="col-md-6 col-lg-5">
-        <h3>Bewaar herinneringen in een digitaal fotoalbum</h3>
-        <p>In de Prisma app heb je als familie een cruciale rol als verhaalverteller. Sla met je smartphone en tablet familie foto's veilig op in een privé archief.</p>
-      </div>
-    </div>
-
   </section>
 
 </div>
 
+<div class="container">
+  <section id="artikels">
+    @php $my_query = new WP_Query(['posts_per_page'=>1, 'post_status'=>'publish']); @endphp
+    @while ($my_query->have_posts())
 
-<section id="download">
+      @php $my_query->the_post(); @endphp
 
-  <h2 class="mb-1 text-center">Hoe werkt de app?</h2>
-  <p class="lead text-center mb-5">Bekijk de video</p>
-
-  <div class="container">
-    <div class="row justify-content-md-center">
-
-      <div class="col-lg-10 col-xl-8 text-center">
-
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe width="560" height="315" src="//www.youtube.com/embed/KrOCPZn8g44?rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>
+      <div class="row my-5">
+        <div class="col-md-4 col-sm-4">
+          <img src="{{ get_the_post_thumbnail_url() }}" alt="" class="img-fluid">
         </div>
-
-        <a href="https://play.google.com/store/apps/details?id=care.prisma.familyapp"><img src="@asset('images/cta-googleplay.png')" alt="Download de app op Google Play" class="playstore-link mt-5"></a>
-
-        <p class="small">Binnenkort beschikbaar voor iPhone en iPad. <br>
-          <a href="#mce-EMAIL">Blijf op de hoogte</a>.</p>
-
+        <div class="col-md-8 col-sm-8">
+          <small class="text-uppercase mb-5">Artikel</small>
+          <h3 class="mt-0 mb-1">
+            <a href="{{ get_permalink() }}">{{ the_title() }}</a>
+          </h3>
+          <p class="d-none d-sm-none d-lg-block">{{ get_the_excerpt() }}</p>
+          {{--<p>--}}
+            {{--<a href="{{ get_permalink() }}">Lees verder</a>--}}
+          {{--</p>--}}
         </div>
       </div>
-    </div>
-
+    @endwhile
   </section>
+</div>
 
-  @endsection
+@endsection
