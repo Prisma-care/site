a86a745bc3b50380efbcf0735e2190e8cae2f269
Frederik Vincx <frederik.vincx@gmail.com>
2018-03-16 11:30:31 +0100

    Refine updates overview and detail

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,9 +1,21 @@
 <div class="col-md-10">
     <article @php(post_class())>
+
         <header>
-            <h1 class="entry-title">{{ get_the_title() }}</h1>
-            @include('partials/entry-meta')
+            <div class="row">
+                <div class="col-lg-7 col-xl-8">
+                    <h1 class="entry-title">{{ get_the_title() }}</h1>
+                    @include('partials/entry-meta')
+                </div>
+
+                <div class="col-sm-8 col-lg-5 col-xl-4">
+                    <img src="{{ get_the_post_thumbnail_url() }}" alt="" class="img-fluid">
+                </div>
+
+            </div>
+
         </header>
+
         <div class="entry-content">
             @php(the_content())
         </div>
--- resources/views/template-artikels.blade.php
+++ resources/views/template-artikels.blade.php
@@ -1,10 +1,10 @@
 {{--
   Template Name: Artikels
---}}
+  --}}
 
-@extends('layouts.simple')
+  @extends('layouts.simple')
 
-@section('content')
+  @section('content')
 
   @php $my_query = new WP_Query(array('posts_per_page'=>-1, 'post_status'=>'publish')); @endphp
 
@@ -13,17 +13,27 @@
 
       @while ($my_query->have_posts())
 
-        @php $my_query->the_post(); @endphp
+      @php $my_query->the_post(); @endphp
 
-        <li>
-          <h2 class="newslist__title"><a href="{{ get_permalink() }}">{{ the_title() }}</a></h2>
-          <span class="newslist__date">{{ the_time('j M y') }}</span>
-          <div class="newslist__content mt-4">{{ the_excerpt() }} <a href="{{ get_permalink() }}">Lees meer</a></div>
-        </li>
+      <li>
+        <div class="row">
+          <div class="col-md-8 col-sm-8">
+            <h2 class="newslist__title"><a href="{{ get_permalink() }}">{{ the_title() }}</a></h2>
+            <span class="newslist__date">{{ the_time('j M y') }}</span>
+            <div class="newslist__content mt-4 d-none d-sm-none d-lg-block">{{ the_excerpt() }} <a href="{{ get_permalink() }}">Lees meer</a></div>
+          </div>
+
+          <div class="col-md-4 col-sm-4">
+            <img src="{{ get_the_post_thumbnail_url() }}" alt="" class="img-fluid">
+          </div>
+
+        </div>
+
+      </li>
 
       @endwhile
 
     </ul>
   </div>
 
-@endsection
+  @endsection
