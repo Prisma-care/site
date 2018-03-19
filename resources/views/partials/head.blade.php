0658c6909a40f6300c38bd964634875674b6ed0f
Frederik Vincx <frederik.vincx@gmail.com>
2018-03-19 16:04:59 +0100

    Remove fixed meta tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,48 +3,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta property="og:title" content="Prisma – Kleur jullie herinneringen">
-  <meta property="og:description" content="Prisma - Bewaar jullie vluchtige herinneringen in een digitaal fotoalbum. Herbeleef samen mooie momenten.">
-  <meta property="og:url" content="https://prisma.care/">
-  <meta name="twitter:card" content="summary_large_image">
 
-  <meta property="og:site_name" content="Prisma – Kleur jullie herinneringen">
-  <meta name="twitter:image:alt" content="Prisma – Kleur jullie herinneringen">
-
-  <title>Prisma – Kleur jullie herinneringen</title>
-  <link rel="apple-touch-icon" sizes="57x57" href="./favicon/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="./favicon/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="./favicon/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="./favicon/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="./favicon/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="./favicon/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="./favicon/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="./favicon/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="./favicon/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="./favicon/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
-  <link rel="manifest" href="./favicon/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="./favicon/ms-icon-144x144.png">
-  <meta name="theme-color" content="#ffffff">
   <link rel="stylesheet" href="./css/app.css?r345">
 
   <script>
-/**
-* Function that tracks a click on an outbound link in Analytics.
-* This function takes a valid URL string as an argument, and uses that URL string
-* as the event label. Setting the transport method to 'beacon' lets the hit be sent
-* using 'navigator.sendBeacon' in browser that support it.
-*/
-var trackOutboundLink = function(url) {
-  ga('send', 'event', 'outbound', 'click', url, {
-    'transport': 'beacon',
-    'hitCallback': function(){document.location = url;}
-  });
-}
-</script>
+  /**
+  * Function that tracks a click on an outbound link in Analytics.
+  * This function takes a valid URL string as an argument, and uses that URL string
+  * as the event label. Setting the transport method to 'beacon' lets the hit be sent
+  * using 'navigator.sendBeacon' in browser that support it.
+  */
+  var trackOutboundLink = function(url) {
+    ga('send', 'event', 'outbound', 'click', url, {
+      'transport': 'beacon',
+      'hitCallback': function(){document.location = url;}
+    });
+  }
+  </script>
 
 @php(wp_head())
+
 </head>
