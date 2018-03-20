5d58f6f370e756b608b00e08602ff62d82bf8233
Frederik Vincx <frederik.vincx@gmail.com>
2018-03-20 14:33:29 +0100

    Add sidebar cta to blog posts

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -4,12 +4,17 @@
         <header>
             <div class="row">
                 <div class="col-lg-7 col-xl-8">
-                    <h1 class="entry-title">{{ get_the_title() }}</h1>
+                    <h1 class="h2 entry-title">{{ get_the_title() }}</h1>
                     @include('partials/entry-meta')
                 </div>
 
                 <div class="col-sm-8 col-lg-5 col-xl-4">
-                    <img src="{{ get_the_post_thumbnail_url() }}" alt="" class="img-fluid">
+                    <div class="aside">
+                        <img src="{{ get_the_post_thumbnail_url() }}" alt="" class="img-fluid">
+                        <p class="text-center">
+                            <a href="/aan-de-slag-met-prisma/" class="btn btn-default">Aan de slag met Prisma</a>
+                        </p>
+                    </div>
                 </div>
 
             </div>
@@ -17,7 +22,11 @@
         </header>
 
         <div class="entry-content">
-            @php(the_content())
+            <div class="row">
+                <div class="col-lg-9 col-xl-8">
+                    @php(the_content())
+                </div>
+            </div>
         </div>
         <footer>
             {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
